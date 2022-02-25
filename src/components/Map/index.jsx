c9aa08794f068d76219ddb97995a19ae108535d2
vichnyi-myzuka <62535515+vichnyi-myzuka@users.noreply.github.com>
2022-02-25 14:27:41 +0200

    feature: chaged assets

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,10 +1,13 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useState } from "react";
 import Places from "../../places";
+import L from "leaflet";
 import SimplePlaces from "../../simple_places";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
 export default function Map() {
-	var options = {
+	let options = {
 		enableHighAccuracy: true,
 		timeout: 5000,
 		maximumAge: 0,
@@ -13,6 +16,12 @@ export default function Map() {
 	const [latitude, setLatitude] = useState(48.5132);
 	const [longitude, setLongitude] = useState(32.2597);
 
+	let DefaultIcon = L.icon({
+		iconUrl: icon,
+		shadowUrl: iconShadow,
+	});
+
+	L.Marker.prototype.options.icon = DefaultIcon;
 	const [coords, setCoords] = useState([latitude, longitude]);
 	function success(pos) {
 		let { latitude, longitude, accuracy } = pos.coords;
